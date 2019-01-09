f03b5fc0d3c35a6d5da7b0ad7f33c331003b39f1
Simon Tomlinson <simonbtomlinson@gmail.com>
2019-01-09 15:30:54 -0600

    Remove logging cruft

--- Block/Widget/Button/Toolbar.php
+++ Block/Widget/Button/Toolbar.php
@@ -12,7 +12,6 @@ class Toolbar
     public function __construct(\Psr\Log\LoggerInterface $logger)
     {
         $this->logger = $logger;
-        $this->logger->debug('Constructing toolbar');
     }
 
     public function beforePushButtons(
@@ -20,18 +19,13 @@ class Toolbar
         \Magento\Framework\View\Element\AbstractBlock $context,
         \Magento\Backend\Block\Widget\Button\ButtonList $buttonList
     ) {
-        $this->logger->debug('In beforePushButtons');
-        $this->logger->debug('Context has class ' . get_class($context));
-        $this->logger->debug('ButtonList: ' . print_r($buttonList->getItems(), true));
         // Only change the toolbar we want to change (the one for orders in the admin)
         if (!$context instanceof \Magento\Sales\Block\Adminhtml\Order\View) {
-            $this->logger->debug('Toolbar was not the admin sales toolbar, no-oping');
             return [$context, $buttonList];
         }
         $order = $context->getOrder();
         // Only change the toolbar if it's an apruve order
         if($order->getPayment()->getMethod() != 'apruve') {
-            $this->logger->debug('Order was not an apruve order, no-oping');
             return [$context, $buttonList];
         }
         
