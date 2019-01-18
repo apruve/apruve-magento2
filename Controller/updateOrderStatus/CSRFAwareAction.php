32f89c46e96af416082fe20050d654967eef6672
Simon Tomlinson <simonbtomlinson@gmail.com>
2019-01-18 13:17:29 -0600

    Attempt to patch magento 2.3 csrf issues

---
+++ Controller/updateOrderStatus/CSRFAwareAction.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Magento 2.3 requires that post actions tell it that they want to handle CSRF themselves, but this is incompatible with
+ * Magneto 2.2 or earlier. To fix this, we dynamically provide two different action base classes depending on the version
+ * of Magento.
+ *
+ * See https://magento.stackexchange.com/a/255082
+ */
+
+namespace Apruve\Payment\Controller\updateOrderStatus;
+
+function shouldApplyCSRFPatch()
+{
+    $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+    $productMetadata = $objectManager->get('Magento\Framework\App\ProductMetadataInterface');
+    $version = $productMetadata->getVersion();
+    preg_match('/2\.(\d)/', $version, $matches); // Try to extract the magento version
+    $minor_version_string = $matches[1];
+    return ((int) $minor_version_string) >= 3;
+}
+
+if (shouldApplyCSRFPatch()) {
+    class CSRFAwareAction extends \Magento\Framework\App\Action\Action implements CsrfAwareActionInterface
+    {
+        public function createCsrfValidationException(RequestInterface $request)
+        {
+            return null;
+        }
+
+        public function validateForCsrf(RequestInterface $request)
+        {
+            return true;
+        }
+    }
+} else {
+    class CSRFAwareAction extends \Magento\Framework\App\Action\Action { }
+}
